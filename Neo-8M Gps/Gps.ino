9bb2d285a19b53bef5d95f7a581a8cb75f54364c
Berat Erkek <erkek.berat@gmail.com>
2023-09-17 21:05:45 +0300

    Codes

---
+++ Neo-8M Gps/Gps.ino
@@ -0,0 +1,112 @@
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+static const int RXPin = 50, TXPin = 52;
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+const long SERIAL_REFRESH_TIME = 100;
+long refresh_time;
+static const int MAX_SATELLITES = 40;
+
+TinyGPSCustom totalGPGSVMessages(gps, "GPGSV", 1); // $GPGSV sentence, first element
+TinyGPSCustom messageNumber(gps, "GPGSV", 2);      // $GPGSV sentence, second element
+TinyGPSCustom satsInView(gps, "GPGSV", 3);         // $GPGSV sentence, third element
+TinyGPSCustom satNumber[4]; // to be initialized later
+TinyGPSCustom elevation[4];
+TinyGPSCustom azimuth[4];
+TinyGPSCustom snr[4];
+struct
+{
+  bool active;
+  int elevation;
+  int azimuth;
+  int snr;
+} sats[MAX_SATELLITES];
+void setup() {
+ Serial.begin(115200);
+  ss.begin(GPSBaud);
+  for (int i=0; i<4; ++i)
+  {
+    satNumber[i].begin(gps, "GPGSV", 4 + 4 * i); // offsets 4, 8, 12, 16
+    elevation[i].begin(gps, "GPGSV", 5 + 4 * i); // offsets 5, 9, 13, 17
+    azimuth[i].begin(  gps, "GPGSV", 6 + 4 * i); // offsets 6, 10, 14, 18
+    snr[i].begin(      gps, "GPGSV", 7 + 4 * i); // offsets 7, 11, 15, 19
+  }
+}
+
+void loop() {
+  ss.listen();
+ if (ss.available() > 0)
+  {
+    gps.encode(ss.read());
+    if (totalGPGSVMessages.isUpdated())
+    {
+      for (int i=0; i<4; ++i)
+      {
+        int no = atoi(satNumber[i].value());
+        // Serial.print(F("SatNumber is ")); Serial.println(no);
+        if (no >= 1 && no <= MAX_SATELLITES)
+        {
+          sats[no-1].elevation = atoi(elevation[i].value());
+          sats[no-1].azimuth = atoi(azimuth[i].value());
+          sats[no-1].snr = atoi(snr[i].value());
+          sats[no-1].active = true;
+        }
+      }    
+      int totalMessages = atoi(totalGPGSVMessages.value());
+      int currentMessage = atoi(messageNumber.value());
+      if (totalMessages == currentMessage)
+      {
+        Serial.print(F("Sats=")); Serial.print(gps.satellites.value());
+        Serial.print(F(" Nums="));
+        for (int i=0; i<MAX_SATELLITES; ++i)
+          if (sats[i].active)
+          {
+            Serial.print(i+1);
+            Serial.print(F(" "));
+          }
+        Serial.print(F(" Elevation="));
+        for (int i=0; i<MAX_SATELLITES; ++i)
+          if (sats[i].active)
+          {
+            Serial.print(sats[i].elevation);
+            Serial.print(F(" "));
+          }
+        Serial.print(F(" Azimuth="));
+        for (int i=0; i<MAX_SATELLITES; ++i)
+          if (sats[i].active)
+          {
+            Serial.print(sats[i].azimuth);
+            Serial.print(F(" "));
+          }
+        
+        Serial.print(F(" SNR="));
+        for (int i=0; i<MAX_SATELLITES; ++i)
+          if (sats[i].active)
+          {
+            Serial.print(sats[i].snr);
+            Serial.print(F(" "));
+          }
+        Serial.println();
+
+        for (int i=0; i<MAX_SATELLITES; ++i)
+          sats[i].active = false;
+      }
+    }       
+          Serial.print("Hız: ");
+          Serial.print(gps.speed.kmph(), 2);
+          delay(1000);
+  }
+          Serial.print("Hız: ");
+          Serial.println(gps.speed.kmph(), 2);
+   smartDelay(200);
+}
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
